db5d3a2c63ffabe7dd198c4e77f543dc7a69ec28
Thinkback2025 <thinkbacktechnologies@gmail.com>
2025-08-11 10:11:32 +0530

    Add files via upload

--- app/src/main/java/com/knets/jr/MainActivity.kt
+++ app/src/main/java/com/knets/jr/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.knets.jr
 
+import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.LinearLayout
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        // Simple layout with basic functionality
-        setContentView(android.R.layout.activity_list_item)
+        // Create simple layout programmatically
+        val layout = LinearLayout(this)
+        layout.orientation = LinearLayout.VERTICAL
         
-        Toast.makeText(this, "Knets Jr - Device Management App", Toast.LENGTH_LONG).show()
+        val textView = TextView(this)
+        textView.text = "Knets Jr - Family Device Management"
+        textView.textSize = 18f
+        textView.setPadding(40, 40, 40, 40)
+        
+        layout.addView(textView)
+        setContentView(layout)
+        
+        Toast.makeText(this, "Knets Jr App Launched Successfully", Toast.LENGTH_LONG).show()
     }
 }
